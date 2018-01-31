f1a66f1c1eae9b049fc0bc44ec8a561a5d3266b2
Isiah Rosa <zker49@gmail.com>
2018-01-31 12:26:35 -0500

    Initial commit GitKraken

---
+++ BounceAdventure_GameDev/Assets/Scripts/MouseControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    private Vector3 startingPosition;
+    private Vector3 offset;
+    private Vector3 releasePosition;
+    public Rigidbody2D rb;
+    public float magnitudeY;
+    public float magnitudeX;
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    /// <summary>
+    /// This is a Unity defined function that is called whenever the mouse is clicked
+    /// </summary>
+    void OnMouseDown()
+    {
+        startingPosition = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+                                                                                            Input.mousePosition.y, 
+                                                                                            startingPosition.z));
+        rb.velocity = new Vector2(0, 0);
+    }
+
+    /// <summary>
+    /// This is a Unity defined function that is called whenever we click and drag the mouse
+    /// </summary>
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startingPosition.z);
+          
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseUp()
+    {
+        releasePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startingPosition.z);
+
+        float magnitudeX = startingPosition.x - releasePosition.x;
+        float magnitudeY = startingPosition.y - releasePosition.y;
+
+        rb.AddForce(new Vector2(magnitudeX, magnitudeY));
+    }
+
+}
---
+++ BounceAdventure_GameDev/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float moveSpeed = 5f; 
+    //public GameObject eatFace;
+    public CircleCollider2D ballCollider;
+    public bool canMove;
+    public float position;
+    Rigidbody2D rb2D;
+    
+    // Use this for initialization
+    void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+        canMove = true;
+    }
+
+    private void Move(float dx, float dy)
+    {
+        rb2D.velocity = new Vector2(dx * moveSpeed, dy * moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (canMove)
+        {
+            Move(x, y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            canMove = !canMove;
+        }
+
+       
+       
+
+    }
+}
